4dc24cf29056ebf66194841242168f4f4b69c8cf
karthick <karthickkavin004@gmail.com>
2024-03-26 17:24:18 +0530

    commit

--- server.js
+++ server.js
@@ -8,7 +8,7 @@ const app = express();
 
 app.use(bodyParser.json());
 
-const PORT = process.env.PORT;
+const PORT = process.env.PORT || 3000;
 const DB_URL = process.env.DB_URL;
 
 //Connecting to the MongoDB
